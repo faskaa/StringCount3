a802a03f922e0d5732aea769128a56962eb11b0a
faskaa <fazilnb@code.edu.az>
2023-08-28 16:42:44 +0400

    first commit initizailed

---
+++ StringCount3/StringCount3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringCount3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello world";
+            char[] b = a.ToCharArray();
+            for (int i = b.Length-1 ; i >=0; i--) 
+            {
+                Console.Write(b[i]);
+            }
+            
+
+
+
+
+        }
+    }
+}
